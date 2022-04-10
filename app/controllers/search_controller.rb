31187f4d2a59bcac13e666f759d9ac3bd5ec2384
Houda Bouamor <ladyhodhod@gmail.com>
2022-04-10 10:32:11 +0300

    updated a few requirements

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,13 +1,5 @@
 class SearchController < ApplicationController
   def basic_search
-    redirect_back(fallback_location: home_path) if params[:query].blank?
-    @query = params[:query]
-    @animals= Animal.search(@query)
-    @total_hits =@animals.size
-    if logged_in? && current_user.role?(:vet)
-      @pets = Pet.search(@query)    
-      @owners = Owner.search(@query)    
-      @total_hits =@animals.size+@pets.size + @owners.size
-    end
+  
   end
 end
