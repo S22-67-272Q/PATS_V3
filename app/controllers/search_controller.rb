8d28130fa971ec2d22b80b1745fb4d6af96a7ee2
Houda Bouamor <ladyhodhod@gmail.com>
2022-04-10 10:58:54 +0300

    updated the search function

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,20 @@
 class SearchController < ApplicationController
   def basic_search
-  
+    redirect_back(fallback_location: home_path) if params[:query].blank?
+
+    # this is the query
+    @query = params[:query]
+
+    # everyone can search for an animal name, including guests
+    @animals= Animal.search(@query)
+    # counting the results obtained
+    @total_hits =@animals.size
+
+    # If the user is an vet (admin), she/he can search for pets and owners, in addition to the animals
+    if logged_in? && current_user.role?(:vet)
+      @pets = Pet.search(@query)    
+      @owners = Owner.search(@query)    
+      @total_hits =@animals.size+ @pets.size + @owners.size
+    end
   end
 end
